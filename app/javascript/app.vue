59b1b1408f6e5a6acce3e4dbace982eb14b39ad7
ikedakohei <kouhik06@gmail.com>
2021-07-15 16:25:27 +0900

    axios導入して、メモ一覧を表示

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,14 +1,31 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <ul>
+      <li v-for="memo in memos" :key="memo.id">
+        {{ memo.title }}: {{ memo.description }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
+      memos: "memos"
+    }
+  },
+  mounted () {
+    this.setMemo();
+  },
+  methods: {
+    setMemo: function () {
+      axios.get('/api/v1/memos')
+      .then(response => (
+        this.memos = response.data
+      ))
     }
   }
 }
